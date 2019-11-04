fb330d0e3d797f36bc182a42222eb6ffc0f7d2f0
Minh Tri <minhtri3007@hotmail.com>
2019-11-05 00:21:46 +0100

    Initial commit First class with random constructor

---
+++ Environnement.py
@@ -0,0 +1,33 @@
+##### IMPORTS #####
+import numpy as np
+import random
+
+
+class ReseauTelecom:
+    """
+    Décris l'environnement
+    contient Steiner Node, target node et les clients
+    contient les coûts de liaison de chaque poste
+    contient la fonction qui calcule le cout total de construction
+
+    Attributs :
+        - steiners : numpy array d'un nb fixé de couple, décrivant une coordonnée
+        - targetNodes : numpy array d'un nb fixé de couple, décrivant une coordonnée
+        - clients : numpy array d'un nb fixé de couple, décrivant une coordonnée
+    """
+
+    def __init__(self, nb_steiner=6, nb_targetNode=5, nb_clients=8, grid_size=(10, 10)):
+        """
+        Initialise le Réseau de neurones aléatoirement uniformement sur la grille
+
+        :param nb_steiner: nombre de Steiner node voulus
+        :param nb_targetNode: nombre de Target Node voulus
+        :param nb_clients: nombre de clients voulus
+        :param grid_size: taille de la grille
+        :type grid_size: couple
+        """
+
+        (x, y) = grid_size
+        self.steiners = [(random.uniform(0, x), random.uniform(0, y)) for i in range(nb_steiner)]
+        self.targetNodes = [(random.uniform(0, x), random.uniform(0, y)) for i in range(nb_targetNode)]
+        self.clients = [(random.uniform(0, x), random.uniform(0, y)) for i in range(nb_clients)]
